945d20be0811180371efcd155bbd4c1ff955da8f
Maximiliano MB <maques.mb@gmail.com>
2022-06-10 16:04:38 -0300

    inicio deploy

--- server.js
+++ server.js
@@ -41,6 +41,6 @@ app.put('/image', (req, res) => { image.handleImage(req, res, db)})
 
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
 
-app.listen(5000, () => {
-    console.log('app is running on port 5000');
+app.listen(process.env.PORT || 3000, () => {
+    console.log(`app is running on port ${process.env.PORT}`);
 });
